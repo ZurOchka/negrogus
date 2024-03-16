c4dcc77b101e894128fae43c805587d7d3127aaa
Zur0 <rasputin.egor.05@mail.ru>
2024-03-16 14:54:48 +0500

    Start

---
+++ app/src/main/java/com/example/practice9my/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.practice9my
+
+import android.graphics.ImageDecoder
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val imageView: ImageView = findViewById(R.id.richad)
+        val chadd: Animation = AnimationUtils.loadAnimation(this, R.anim.chad)
+        imageView.startAnimation(chadd)
+
+        val imageView2: ImageView = findViewById(R.id.silcat)
+        val silcat: Animation = AnimationUtils.loadAnimation(this, R.anim.sillybr)
+        imageView2.startAnimation(silcat)
+
+        val imageView3: ImageView = findViewById(R.id.pawl)
+        val smk: Animation = AnimationUtils.loadAnimation(this, R.anim.smekh)
+        imageView3.startAnimation(smk)
+    }
+
+}
